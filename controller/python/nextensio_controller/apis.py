c0c2b088f28f5792e57cfe215d1133eaf2b2a4ad
Gopa Kumar <gopakumarce@gmail.com>
2021-04-08 15:19:14 -0400

    change the api URLs to match the controller

--- controller/python/nextensio_controller/apis.py
+++ controller/python/nextensio_controller/apis.py
@@ -4,23 +4,23 @@ import time
 import json
 import os
 
-def doGet(url):
+def doGet(url, path):
     cert = os.getenv('NEXTENSIO_CERT') 
     if cert == None:
-        return requests.get(url)
+        return requests.get(url + "/api/v1/" + path)
     else:
-        return requests.get(url, verify=cert)
+        return requests.get(url + "/api/v1/" + path, verify=cert)
 
-def doPost(url, data):
+def doPost(url, data, path):
     cert = os.getenv('NEXTENSIO_CERT') 
     if cert == None:
-        return requests.post(url, json=data)
+        return requests.post(url + "/api/v1/" + path, json=data)
     else:
-        return requests.post(url, json=data, verify=cert)
+        return requests.post(url + "/api/v1/" + path, json=data, verify=cert)
 
 def is_controller_up(url):
     try:
-        ret = doGet(url+"getalltenants")
+        ret = doGet(url, "global/get/alltenants")
         return (ret.status_code == 200)
     except:
         pass
@@ -29,7 +29,7 @@ def is_controller_up(url):
 
 def do_post(url, data, suffix):
     try:
-        ret = doPost(url+suffix, data)
+        ret = doPost(url, data, suffix)
         if ret.status_code != 200 or ret.json()['Result'] != "ok":
             return False
         return True
@@ -39,15 +39,15 @@ def do_post(url, data, suffix):
 
 
 def create_gateway(url, data):
-    return do_post(url, data, "addgateway")
+    return do_post(url, data, "global/add/gateway")
 
 def create_tenant(url, data):
-    return do_post(url, data, "addtenant")
+    return do_post(url, data, "global/add/tenant")
 
 
 def get_tenants(url):
     try:
-        ret = doGet(url+"getalltenants")
+        ret = doGet(url, "global/get/alltenants")
         if ret.status_code != 200:
             return False, json.dumps([])
         return True, ret.json()
@@ -56,36 +56,36 @@ def get_tenants(url):
         return False, json.dumps([])
 
 
-def create_user(url, data):
-    return do_post(url, data, "adduser")
+def create_user(url, tenant, data):
+    return do_post(url, data, "tenant/%s/add/user" % tenant)
 
-def create_bundle(url, data):
-    return do_post(url, data, "addbundle")
+def create_bundle(url, tenant, data):
+    return do_post(url, data, "tenant/%s/add/bundle" % tenant)
     
-def create_user_attr(url, data):
-    return do_post(url, data, "adduserattr")
+def create_user_attr(url, tenant, data):
+    return do_post(url, data, "tenant/%s/add/userattr" % tenant)
  
-def create_bundle_attr(url, data):
-    return do_post(url, data, "addbundleattr")
+def create_bundle_attr(url, tenant, data):
+    return do_post(url, data, "tenant/%s/add/bundleattr" % tenant)
  
-def create_host_attr(url, data):
-    return do_post(url, data, "addhostattr")
+def create_host_attr(url, tenant, data):
+    return do_post(url, data, "tenant/%s/add/hostattr" % tenant)
  
-def create_userext_attr(url, data):
-    return do_post(url, data, "adduserextattr")
+def create_userext_attr(url, tenant, data):
+    return do_post(url, data, "tenant/%s/add/userextattr" % tenant)
 
 def create_policy(url, tenant, pid, policy):
     rego = []
     for p in policy:
         rego.append(ord(p))
-    data = {'tenant': tenant, 'pid': pid, 'rego': rego}
-    return do_post(url, data, "addpolicy")
+    data = {'pid': pid, 'rego': rego}
+    return do_post(url, data, "tenant/%s/add/policy" % tenant)
 
 
-def create_route(url, data):
-    return do_post(url, data, "addroute")
+def create_route(url, tenant, data):
+    return do_post(url, data, "tenant/%s/add/route" % tenant)
 
 
 def create_cert(url, cert):
     data = {'certid': 'CACert', 'cert': [ord(c) for c in cert]}
-    return do_post(url, data, "addcert")
+    return do_post(url, data, "global/add/cert")
